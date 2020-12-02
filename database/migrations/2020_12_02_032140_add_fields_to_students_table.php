d077065695f5f3268a0a8806a83210b2256bf25a
inseasec <inseasec@gmail.com>
2020-12-02 13:58:32 -0800

    time added

--- database/migrations/2020_12_02_032140_add_fields_to_students_table.php
+++ database/migrations/2020_12_02_032140_add_fields_to_students_table.php
@@ -26,6 +26,7 @@ class AddFieldsToStudentsTable extends Migration
             $table->string('hobby');
             $table->string('Courses');
             $table->time('Time');
+            $table->time('Time');
 
         });
     }
