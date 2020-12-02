5015106be1f13c788d8332159cdcfe24153874ce
inseasec <inseasec@gmail.com>
2020-12-01 19:41:53 -0800

    Migrations(Jaspreet)

---
+++ database/migrations/2020_12_02_032140_add_fields_to_students_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFieldsToStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('students', function (Blueprint $table) {
+
+            $table->string('Name');
+            $table->string('Email');
+            $table->integer('Number');
+            $table->string('Password');
+            $table->string('confirm_pass');
+            $table->date('Birthday');
+            $table->date('Date');
+            $table->string('Gender');
+            $table->string('hobby');
+            $table->string('Courses');
+            $table->time('Time');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            //
+        });
+    }
+}
