05e9f2fb0feab1b06327fe1fea05f1cb23664092
inseasec <inseasec@gmail.com>
2020-12-07 15:56:07 -0800

    show

--- resources/views/User/show.blade.php
+++ resources/views/User/show.blade.php
@@ -11,105 +11,117 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   </head>
   <style>
-    .data{
-      display: none;
+
+    table{
+        width: 100%;
+        border: 1px solid black;
     }
+    
+    td, th{
+        border: 1px solid black;
+        text-align:left;
+        color: black;
+        padding: 8px;
+    }
+
+    tr:nth-child(even){
+        background-color: pink;
+    }
+
+    .details,
+    .details div{
+        display:none;
+    }
+
   </style>
   <script>
-  // $(document).ready(function(){
-  //   $(".show_hide").click(function(){
-  //     var tableData = $('.show_hide').closest('tr').children("td").map(function(){
-  //       return $(this).text();
-  //     }).get();
-  //     $('.data').slideToggle("slow").css("color", "orange");
-  //     // alert(tableData[0] + " , " + tableData[1]);
-  //   });
-  // });
-
     $(document).ready(function(){
-      $(".show_hide").click(function(){
-        $(this).closest("tr").find('.data').slideToggle().css("color", "orange");
-      });
-    });
+        $('.show_hide').on('click', function(){
+            var row = $(this).closest('tr').next('.details');
+            row.show().find('div').slideToggle('slow');
+            });
+        });
 
   </script>
 <body>
   <div id="error"></div> 
-      <a href='create' class="btn btn-lg btn btn-primary mt-5 ml-5 ">Add Record</a>
-      <table class= "table table-dark mt-5">
-        <thead>
+    <a href='create' class="btn btn-lg btn btn-primary mt-5 ml-5 mb-5">Add Record</a>
+    <table class= "table">
         <tr>
-          <th class="data">Id</th>
-          <th>Name</th>
-          <th class="data">Email</th>
-          <th class="data">Password</th>
-          <th class="data">Number</th>
-          <th class="data">Confirm Password</th>
-          <th class="data">Birthday</th>
-          <th class="data">Date</th>
-          <th class="data">Gender</th> 
-          <th class="data">Hobbies</th> 
-          <th class="data">Courses</th> 
-          <th class="data">Time</th> 
-          <th class="data text-center">Action</th>
-          </tr>
-        </thead>
-
-        <tbody>
+            <th>Name</th>
+            <th colspan="11" class="text-center mx-auto">Actions</th>
+        </tr>
 
         @foreach($users as $user)
+        <tr>
+            <td>{{$user->Name}}</td>
+            <td><button class="show_hide btn btn-primary mx-auto d-block">Show/Hide Records</button></td>
+            <td style="text-align:center;"><a href='edit/{{$user->id}}' class="btn btn-primary">Update</a></td>
+            <td><button class="btn btn-danger mx-auto d-block" id="delete" data-Id="{{ $user->id }}">Delete</button></td>
+        </tr>
+
+        <tr class="details">
+        <td colspan="11">
+            <div>
+            <table class="table">
+            <thead>
+            <th>Id</th>
+            <th>Email</th>
+            <th>Password</th>
+            <th>Number</th>
+            <th>Confirm Password</th>
+            <th>Birthday</th>
+            <th>Date</th>
+            <th>Gender</th> 
+            <th>Hobbies</th> 
+            <th>Courses</th> 
+            <th>Time</th> 
+            </thead>
         
         <tr id="row">
-        <td class="data">{{$user->id}}</td>
-        <td>{{$user->Name}}</td>
-        <td class="data">{{$user->Email}}</td>
-        <td class="data">{{$user->Password}}</td>
-        <td class="data">{{$user->Number}}</td>  
-        <td class="data">{{$user->confirm_Pass}}</td>
-        <td class="data">{{$user->Birthday}}</td>
-        <td class="data">{{$user->Date}}</td> 
-        <td class="data">{{$user->Gender}}</td> 
-        <td class="data">{{$user->hobby}}</td>
-          
-        <td class="data">{{$user->Courses}}</td>  
-        <td class="data">{{$user->Time}}</td>
-        
-        <td><button class="show_hide btn btn-primary">Show/Hide Records</button><td>
-        <td><a href='edit/{{$user->id}}' class="btn btn-primary" >Update</a></td>
-
-      <!-- <td><a href='delete/{{$user->id}}' class="btn btn-danger" onclick="return confirm('You really want to delete this record?')">Delete</a></td> -->
-
-      <td><button class="btn btn-danger" id="delete" data-Id="{{ $user->id }}">Delete</button></td>
-      
-
+            <td>{{$user->id}}</td>
+            <td>{{$user->Email}}</td>
+            <td>{{$user->Password}}</td>
+            <td>{{$user->Number}}</td>  
+            <td>{{$user->confirm_Pass}}</td>
+            <td>{{$user->Birthday}}</td>
+            <td>{{$user->Date}}</td> 
+            <td>{{$user->Gender}}</td> 
+            <td>{{$user->hobby}}</td>
+            <td>{{$user->Courses}}</td>  
+            <td>{{$user->Time}}</td>
         </tr>
-        @endforeach
-        </tbody>
-      
-      </table>
+
+    </table>
+    </div>
+    <td>
+    </tr>
+
+    @endforeach
+    </table>
 
       <script>
       $('td #delete').on('click',function(){
 
         var Id = $(this).data("id");
-        
+
         var Id = $(this).attr('data-Id');
         // alert(Id);
         $.ajax({
-          url:"delete/"+Id,
-          type:"get",
-          success:function(data){
-            if(data == 'deleted'){
-              // window.location.reload();
-              $('#row').remove(); 
-            }else{
-              $('#error').show();
-              $('#error').html('Something went wrong');
-            }
-          }
-        });
+        url:"delete/"+Id,
+        type:"get",
+        success:function(data){
+        if(data == 'deleted'){
+        window.location.reload();
+        //  $('#row').remove(); 
+        }else{
+        $('#error').show();
+        $('#error').html('Something went wrong').css("color: orange");
+        }
+        }
+    });
 
-      });
+});
 
 </script>   
 
